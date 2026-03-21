f917802b8f51be563d52b6ffa591f7a60d8d9679
unknown <1321090317@qq.com>
2026-03-21 13:18:46 +0800

    Animation import and Basic Move

---
+++ Source/TPS/Public/Character/TPSMainCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/TPSCharacterBase.h"
+#include "TPSMainCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_API ATPSMainCharacter : public ATPSCharacterBase
+{
+	GENERATED_BODY()
+	
+};
